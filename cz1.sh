44e88929e13f18aa1023609bca274576c590a5d7
Denys Kotsiuba <nikolay12345678917@gmail.com>
2023-05-21 20:10:16 +0200

    update

--- cz1.sh
+++ cz1.sh
@@ -1,3 +1,24 @@
+if [[ $1 == "--date" ]]; then
+    # Wyświetlanie dzisiejszej daty
+    echo $(date +"%Y-%m-%d")
+elif [[ $1 == "--logs" ]]; then
+    if [[ -n $2 && $2 =~ ^[0-9]+$ ]]; then
+        # Utworzenie określonej liczby plików log
+        for ((i=1; i<=$2; i++)); do
+            filename="log$i.txt"
+            echo "Nazwa pliku: $filename" > $filename
+            echo "Nazwa skryptu: $0" >> $filename
+            echo "Data: $(date +"%Y-%m-%d")" >> $filename
+        done
+    else
+        # Utworzenie 100 plików log
+        for ((i=1; i<=100; i++)); do
+            filename="log$i.txt"
+            echo "Nazwa pliku: $filename" > $filename
+            echo "Nazwa skryptu: $0" >> $filename
+            echo "Data: $(date +"%Y-%m-%d")" >> $filename
+        done
+    fi
 elif [[ $1 == "--help" ]]; then
     # Wyświetlanie dostępnych opcji
     echo "Dostępne opcje:"
