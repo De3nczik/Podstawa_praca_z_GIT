fa66d35c0d8f72caf4320d16d2430a7983c24bd5
De3nczik <134198041+De3nczik@users.noreply.github.com>
2023-05-22 18:38:43 +0200

    Update cz1.sh

--- cz1.sh
+++ cz1.sh
@@ -1,5 +1,5 @@
 elif [[ $1 == "--help" ]]; then
-    # Wyświetlanie dostępnych opcji
+    
     echo "Dostępne opcje:"
     echo "--date: Wyświetla dzisiejszą datę."
     echo "--logs [liczba]: Tworzy pliki log. Jeśli liczba jest podana, tworzy tyle plików, w przeciwnym razie tworzy 100 plików."
