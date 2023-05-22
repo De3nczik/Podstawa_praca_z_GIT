947f2c4d70c2835b889564c927d745ffd55c7c78
De3nczik <134198041+De3nczik@users.noreply.github.com>
2023-05-22 20:33:37 +0200

    Update cz2.sh

--- cz2.sh
+++ cz2.sh
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 if [[ $1 == "--date" || $1 == "-d" ]]; then
 
     echo $(date +"%Y-%m-%d")
-=======
+
 elif [[ $1 == "--logs" || $1 == "-l" ]]; then
     if [[ -n $2 && $2 =~ ^[0-9]+$ ]]; then
 
@@ -21,25 +21,24 @@ elif [[ $1 == "--logs" || $1 == "-l" ]]; then
             echo "Data: $(date +"%Y-%m-%d")" >> $filename
         done
     fi
->>>>>>> logs
-=======
+
 elif [[ $1 == "--init" ]]; then
     git clone https://github.com/De3nczik/Podstawa_praca_z_GIT.git .
     export PATH=$PATH:$(pwd)
 >>>>>>> init    
 elif [[ $1 == "--error" || $1 == "-e" ]]; then
     if [[ -n $2 && $2 =~ ^[0-9]+$ ]]; then
-        # Utworzenie określonej liczby plików errorx/errorx.txt
+        
         for ((i=1; i<=$2; i++)); do
-            filename="error$i/error$i.txt"
+            filename="error$i.txt"
             echo "Nazwa pliku: $filename" > $filename
             echo "Nazwa skryptu: $0" >> $filename
             echo "Data: $(date +"%Y-%m-%d")" >> $filename
         done
     else
-        # Utworzenie 100 plików errorx/errorx.txt
+        
         for ((i=1; i<=100; i++)); do
-            filename="error$i/error$i.txt"
+            filename="error$i.txt"
             echo "Nazwa pliku: $filename" > $filename
             echo "Nazwa skryptu: $0" >> $filename
             echo "Data: $(date +"%Y-%m-%d")" >> $filename
