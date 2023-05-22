188dd864483e43413ae16949bb51d1a2372760ab
De3nczik <134198041+De3nczik@users.noreply.github.com>
2023-05-22 18:38:17 +0200

    Update cz1.sh

--- cz1.sh
+++ cz1.sh
@@ -1,9 +1,9 @@
 if [[ $1 == "--date" ]]; then
-    # Wyświetlanie dzisiejszej daty
+    
     echo $(date +"%Y-%m-%d")
 elif [[ $1 == "--logs" ]]; then
     if [[ -n $2 && $2 =~ ^[0-9]+$ ]]; then
-        # Utworzenie określonej liczby plików log
+        
         for ((i=1; i<=$2; i++)); do
             filename="log$i.txt"
             echo "Nazwa pliku: $filename" > $filename
@@ -11,7 +11,7 @@ elif [[ $1 == "--logs" ]]; then
             echo "Data: $(date +"%Y-%m-%d")" >> $filename
         done
     else
-        # Utworzenie 100 plików log
+        
         for ((i=1; i<=100; i++)); do
             filename="log$i.txt"
             echo "Nazwa pliku: $filename" > $filename
